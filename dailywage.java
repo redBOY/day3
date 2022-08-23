a660ffc2e7aa599d41e1edf3f98a7172c5a98d58
“Adithya <adithyasrinivas66@gmail.com>
2022-08-23 14:54:45 +0530

    uc2 commit

---
+++ dailywage.java
@@ -0,0 +1,20 @@
+package main.main.day3;
+
+public class dailywage {
+
+    public static void main(String[] args) {
+        int is_full_time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (is_full_time == empCheck) {
+            int empRatePerHour = 20;
+            int empHrs = 8;
+            double dailyEmpWage = (empHrs * empRatePerHour);
+            System.out.println("Daily Employee Wage if Employee is Present=" + dailyEmpWage);
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+    }
+
+
+}
