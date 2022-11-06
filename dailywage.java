b7c0aa118e7efdab3bbe79d40c4bd59d6801b539
redBOY <43438363+redBOY@users.noreply.github.com>
2022-11-06 09:19:18 +0530

    uc2 commit

--- dailywage.java
+++ dailywage.java
@@ -4,10 +4,10 @@ public class dailywage {
 
     public static void main(String[] args) {
         int is_full_time = 1;
+        int empRatePerHour = 20;
+            int empHrs = 8;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (is_full_time == empCheck) {
-            int empRatePerHour = 20;
-            int empHrs = 8;
             double dailyEmpWage = (empHrs * empRatePerHour);
             System.out.println("Daily Employee Wage if Employee is Present=" + dailyEmpWage);
         } else {
